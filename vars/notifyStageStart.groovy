e9fbffeaf82a920edf0b8505e4f56b4d7f168db3
Chris Schreiber <chriss@liatrio.com>
2019-06-20 12:57:03 -0700

    Add pipeline notify methods

---
+++ vars/notifyStageStart.groovy
@@ -0,0 +1,21 @@
+import groovy.json.JsonOutput
+
+def call(params) {
+
+  if (!params) params = [:]
+
+  def request = params.request ? params.request : [:]
+
+  request.type = 'stage-start'
+  request.gitUrl = env.GIT_URL
+  request.gitCommit = env.GIT_COMMIT
+  request.buildId = env.BUILD_ID
+  request.branch = env.BRANCH_NAME
+  request.stage = env.STAGE_NAME
+
+  def requestBody = JsonOutput.toJson(request)
+  def url = (env.jenkinsOperator ? env.jenkinsOperator : "http://localhost:5555/notify-pipeline")
+
+  def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: requestBody, url: url
+  println('Response: (' + response.status + ') ' + response.content)
+}
