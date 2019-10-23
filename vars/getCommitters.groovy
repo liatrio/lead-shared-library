e650e2263ad87c76af11595b3e6b2475847d36fd
Takumi Nishida <takumin@liatrio.com>
2019-10-23 12:17:23 -0700

    adding console statement

--- vars/getCommitters.groovy
+++ vars/getCommitters.groovy
@@ -2,8 +2,10 @@ def call() {
   def log = "git log -n 5".execute()
   def emailLog  = log.text =~ /<(.*@.*)>/
   List<String> userEmailList = new ArrayList<>()
+  println "inside getCommitters function"
   for(email in emailLog){
     userEmailList << email[1]
   }
+  println userEmailList
   return userEmailList
 }
