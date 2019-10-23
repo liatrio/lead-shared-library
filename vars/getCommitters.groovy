da4ec522084980cba137f5f770ce65415db40d5d
Takumi Nishida <takumin@liatrio.com>
2019-10-23 14:22:15 -0700

    changing committers

--- vars/getCommitters.groovy
+++ vars/getCommitters.groovy
@@ -1,7 +1,9 @@
 def call() {
-  def log = "git log -5 | cat".execute()
+  def log = "git log -5".execute() | "cat".execute()
+  log.waitFor()
   println "printing log below:"
   println log
+  println log.text
   def emailLog  = log.text =~ /<(.*@.*)>/
   List<String> userEmailList = new ArrayList<>()
   for(email in emailLog){
