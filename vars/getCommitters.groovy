541bef33815e1319a26b15ae42efcec1aff24192
Takumi Nishida <takumin@liatrio.com>
2019-10-23 15:22:18 -0700

    moving function into notify pipeline start

--- vars/getCommitters.groovy
+++
@@ -1,9 +0,0 @@
-def call() {
-  def log = sh(returnStdout: true, script: "git --no-pager log -5")
-  def emailLog  = log =~ /<(.*@.*)>/
-  List<String> userEmailList = new ArrayList<>()
-  for(email in emailLog){
-    userEmailList << email[1]
-  }
-  return userEmailList
-}
--- vars/notifyPipelineStart.groovy
+++ vars/notifyPipelineStart.groovy
@@ -54,4 +54,12 @@ def getStageNames(pipeline){
   return names
 }
 
-
+def getCommitters() {
+  def log = sh(returnStdout: true, script: "git --no-pager log -5")
+  def emailLog  = log =~ /<(.*@.*)>/
+  List<String> userEmailList = new ArrayList<>()
+  for(email in emailLog){
+    userEmailList << email[1]
+  }
+  return userEmailList
+}
