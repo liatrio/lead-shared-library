8599dcaefd412179a2e742c60af13bab88b68070
Takumi Nishida <takumin@liatrio.com>
2019-10-28 10:44:56 -0700

    test

--- vars/notifyPipelineStart.groovy
+++ vars/notifyPipelineStart.groovy
@@ -19,8 +19,9 @@ def call(params) {
     def Jenkinsfile = params.Jenkinsfile ? params.Jenkinsfile : 'Jenkinsfile'
     def pipeline = readFile file: Jenkinsfile
     request.stageNames = getStageNames(pipeline)
+    
+    request.committers = getCommitters()
 
-    request.put('committers', new JsonBuilder(getCommitters()))
     def requestBody = JsonOutput.toJson(request)
     println JsonOutput.prettyPrint(requestBody)
     sendRequest(requestBody)
